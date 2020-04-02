937cdfa21c61fe716d741174cba9e518e354fb28
Bárbara C. Blank Garibaldi <barbaracbgaribaldi@gmail.com>
2020-04-02 20:58:36 -0300

    update angular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
@@ -13,6 +16,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CommonModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatSidenavModule,
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule
---
+++ frontend/src/app/navigation/sidenav-responsive.component.ts
@@ -0,0 +1,28 @@
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+
+
+@Component({
+  selector: 'sidenav-responsive-component',
+  templateUrl: 'sidenav-responsive-component.html',
+  styleUrls: ['sidenav-responsive-component.css'],
+})
+export class SidenavResponsiveExample implements OnDestroy {
+  mobileQuery: MediaQueryList;
+
+  fillerNav = ["Usuários", "Produtos", "Pedidos"]
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+}
